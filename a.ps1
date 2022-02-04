f313db741eebff53100855c4dfb0bd66ce7754be
Biradar <vb185115@ncr.com>
2022-02-04 12:27:37 +0530

    add b

--- a.ps1
+++ a.ps1
@@ -1,6 +1,1 @@
-Get-WmiObject win32_networkadapterconfiguration | 
-Select-Object -Property @{
-    Name = 'IPAddress'
-    Expression = {($PSItem.IPAddress[0])}
-},MacAddress,hostname | 
-Where-Object IPAddress -NE $null
+Write-host "Hello World"
